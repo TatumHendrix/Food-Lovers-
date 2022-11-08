b21032b7a6dfbda01747f0266f9c53ee267b9331
Patrick <Mallette1313@Gmail.com>
2022-11-08 09:07:01 -0500

    Add files via upload

--- Food Lovers Landing Page-ungdev/slideshow.js
+++ Food Lovers Landing Page-ungdev/slideshow.js
@@ -1,49 +1,84 @@
-const spaghettiDish = document.getElementById("spaghettiDishImg");
-let spdY = spaghettiDish.style.marginTop;
+const spaghetti = document.getElementById("spaghettiDishImg");
+const shrimp = document.getElementById("shrimpDishImg");
+const cheesecake = document.getElementById("cheesecakeDishImg");
+const wine = document.getElementById('wineDishImg');
+const salad = document.getElementById('saladDishImg');
+ 
+let spaghettiMt = spaghetti.style.marginTop;
+let shrimpMt = shrimp.style.marginTop;
+let cheesecakeMt = cheesecake.style.marginTop;
+let wineMt = wine.style.marginTop;
+let saladMt = salad.style.marginTop;
 
-const shrimpDish = document.getElementById("shrimpDishImg");
-let shdY = shrimpDish.style.marginTop;
+let imgArr = [spaghetti, shrimp, cheesecake, wine, salad];
+let mTArr = [spaghettiMt, shrimpMt, cheesecakeMt, wineMt, saladMt];
+let yValues = [-200, 0, 200, 400, 600];
 
-const cheesecakeDish = document.getElementById("cheesecakeDishImg");
-let ccdY = cheesecakeDish.style.marginTop;
+let Img = class {
+    constructor(num) {
+        this.element = imgArr[num];
+        this.mT = mTArr[num];
+        this.y = yValues[num];
+    }
 
-const wineImage = document.getElementById("wideDishImg");
-let wiY = wineImage.style.marginTop;
-
-const saladDish = document.getElementById("saladDishImg");
-let sadY = saladDish.style.marginTop;
-
-let slideInterval = 0;
-
-shrimpDish.style.marginTop = 200 + 'px';
-
-function slideshow() {
-  if (slideInterval < 600) {
-    wiY = slideInterval + 'px';
-    sadY = (slideInterval + 200) + 'px';
-    ccdY = (slideInterval - 200) + 'px';
-    shdY = (slideInterval - 400) + 'px';
-    spdY = (slideInterval - 600) + 'px';
-    return [wiY, sadY, ccdY, shdY, spdY];
-  } else if (slideInterval >= 600 && slideInterval <= 1000) {
-    spdY = (slideInterval - 600) + 'px';
-    shdY = (slideInterval -400 )+ 'px;';
-    sadY = (slideInterval - 800) + 'px';
-    wiY = (slideInterval - 1000) + 'px';
-    return [spdY, shdY, sadY, wiY];
-  } else {
-    slideInterval = 0;
-  }
-  slideInterval++;
+    bump() {
+        return this.y++;
+    }
 }
 
-const slideSpeed = 50;
-
-
-function startSlide() {
-setInterval(slideshow, 10000)
+function bumpAll(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        arr[i].bump();
+    }
+    return arr;
 }
 
-startSlide();
+function placeSlides(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        arr[i].mT = `${arr[i].y}px`
+        arr[i].element.style.marginTop = arr[i].mT
+    }
+    return arr;
+}
 
-console.log(slideInterval);
+function cycle(arr) {
+    arr.unshift(arr[arr.length - 1]);
+    arr.pop();
+    arr[0].y = yValues[0];
+    return arr;
+}
+
+function display(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].y > 400) {
+            arr[i].element.style.display = 'none';
+        } else {
+            arr[i].element.style.display = 'block';
+        }
+    }
+}
+
+function slideShow(arr) {
+    bumpAll(arr);
+    placeSlides(arr);
+    if(arr[0].y >= 0) {
+        return cycle(arr);
+    }
+        display(arr);
+}
+
+const slide0 = new Img(0);
+const slide1 = new Img(1);
+const slide2 = new Img(2);
+const slide3 = new Img(3);
+const slide4 = new Img(4);
+
+const slides = [slide0, slide1, slide2, slide3, slide4];
+
+function imgSlideShow() {
+    return slideShow(slides);
+}
+
+const slider = setInterval(imgSlideShow, 100);
+
+slider;
